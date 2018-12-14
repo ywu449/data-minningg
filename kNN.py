00eeca930bab6d143daa01695945169c4a6ca52f
wuevan <44665950+wuevan@users.noreply.github.com>
2018-12-14 14:36:47 +0800

    1214

--- kNN.py
+++ kNN.py
@@ -39,8 +39,8 @@ def file2matrix(filename):
 		index +=1
 	return returnMat,classLabelVector
 	
-datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.scatter(datingDataMat[:,1],datingDataMat[:,2])
-plt.show()
+# datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# ax.scatter(datingDataMat[:,1],datingDataMat[:,2])
+# plt.show()
