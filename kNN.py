f41fb3682bb63c1ce408588d8c6bb0195f2d7bac
wuevan <44665950+wuevan@users.noreply.github.com>
2018-12-19 20:03:28 +0800

    1219

--- kNN.py
+++ kNN.py
@@ -44,3 +44,12 @@ def file2matrix(filename):
 # ax = fig.add_subplot(111)
 # ax.scatter(datingDataMat[:,1],datingDataMat[:,2])
 # plt.show()
+
+def img2vector(filename):
+	returnVect = zeros((1,1024))
+	fr = open(filename)
+	for i in range(32):
+		lineStr = fr.readline()
+		for j in range(32):
+			returnVect[0,32*i+j] = int(lineStr[j])
+	return returnVect
